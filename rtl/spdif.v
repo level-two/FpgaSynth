0c50f9fbc9ce71acb6b531a798cc8d55341d101b
Yauheni Lychkouski <370903+level-two@users.noreply.github.com>
2017-01-29 19:48:52 +0300

    Reworked spdif.v to have one counter instead of two

--- rtl/spdif.v
+++ rtl/spdif.v
@@ -20,62 +20,47 @@ module spdif (
 );
  
 
-    reg [5:0] bit_cnt;
+    reg [14:0] bit_cnt;
     always @(posedge reset or posedge clk) begin
         if (reset) begin
             bit_cnt <= 0;
         end
         else begin
-            bit_cnt <= bit_cnt + 1'b1;
+            if (bit_cnt == 15'h5FFF)
+                bit_cnt <= 0;
+            else
+                bit_cnt <= bit_cnt + 1'b1;
         end
     end
 
 
-    reg [8:0] sf_cnt;
-    always @(posedge reset or posedge clk) begin
-        if (reset) begin
-            sf_cnt <= 0;
-        end
-        else if (subFrame_trig) begin
-            if (sf_cnt == 9'd383) begin // 192*2-1
-                sf_cnt <= 0;
-            end
-            else begin
-                sf_cnt <= sf_cnt + 1'b1;
-            end
-        end
-    end
+    // Channel: 0 - left, 1 - right
+    wire        chanel   =  bit_cnt[6];
+    wire [15:0] sample   = (chanel == 0) ? left_in : right_in;
+    wire [7:0]  preamble = (bit_cnt[14:6] == 9'b0) ? 8'b10011100 : // B (left)
+                           (chanel == 0)           ? 8'b10010011 : // M (left)
+                                                     8'b10010110 ; // W (right)
+    wire        sf_trig  = (bit_cnt[5:0] == 6'b0);
+    wire        parity   = ^sample;
 
-
-    wire        chanel         = sf_cnt[0]; // 0 - left, 1 - right
-    wire [15:0] sample         = (chanel == 0) ? left_in : right_in;
-    wire [7:0]  preamble       = (sf_cnt == 0) ? 8'b10011100 : // B (left)
-                                 (chanel == 0) ? 8'b10010011 : // M (left)
-                                                 8'b10010110 ; // W (right)
-    wire        subFrame_trig  = (bit_cnt == 6'b0);
-
-    reg         parity;
     reg  [63:0] subFrame;
-
     always @(posedge reset or posedge clk) begin
         if (reset) begin
             left_accepted  <= 0;
             right_accepted <= 0;
-            parity   <= 0;
             subFrame <= 0;
         end
-        else if (subFrame_trig) begin
+        else if (sf_trig) begin
             left_accepted  <= ~chanel;
             right_accepted <=  chanel;
 
-            parity   <= ^sample;
             subFrame <= { preamble,
                          16'b1010101010101010,
-                         //      V                 U                 C                 P
                          1'b1, sample[0] , 1'b1, sample[1] , 1'b1, sample[2] , 1'b1, sample[3] ,
                          1'b1, sample[4] , 1'b1, sample[5] , 1'b1, sample[6] , 1'b1, sample[7] ,
                          1'b1, sample[8] , 1'b1, sample[9] , 1'b1, sample[10], 1'b1, sample[11],
                          1'b1, sample[12], 1'b1, sample[13], 1'b1, sample[14], 1'b1, sample[15],
+                         //      V                 U                 C                 P
                          1'b1, 1'b0      , 1'b1, 1'b0      , 1'b1, 1'b0      , 1'b1, parity
                         };
         end
