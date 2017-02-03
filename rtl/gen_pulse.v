75445668d8c3822731653556cce60b165884cbf3
Yauheni Lychkouski <370903+level-two@users.noreply.github.com>
2017-02-04 00:40:42 +0300

    Fixed gen_pulse; Width of the dac input data shrinked by one digit (should help to omit overflows); Debug led outputs

--- rtl/gen_pulse.v
+++ rtl/gen_pulse.v
@@ -30,12 +30,15 @@ module gen_pulse (
     output reg signed [17:0] right_sample_out
 );
 
+    wire note_on_event  = (midi_rdy && midi_cmd == `MIDI_CMD_NOTE_ON);
+    wire note_off_event = (midi_rdy && midi_cmd == `MIDI_CMD_NOTE_OFF);
+
     reg [7:0] note;
     always @(posedge reset or posedge clk) begin
         if (reset) begin
             note <= 0;
         end
-        else if (midi_rdy && midi_cmd == `MIDI_CMD_NOTE_ON) begin
+        else if (note_on_event) begin
             note <= midi_data0;
         end
     end
@@ -43,164 +46,165 @@ module gen_pulse (
     reg [31:0] div;
     always @(note) begin
         case (note)
-            7'h00: div <= 32'h03a52528;
-            7'h01: div <= 32'h0370c590;
-            7'h02: div <= 32'h033f567c;
-            7'h03: div <= 32'h0310adac;
-            7'h04: div <= 32'h02e4a348;
-            7'h05: div <= 32'h02bb11ac;
-            7'h06: div <= 32'h0293d550;
-            7'h07: div <= 32'h026eccb8;
+            7'h00: begin div <= 32'h005d5084; end
+            7'h01: begin div <= 32'h005813c2; end
+            7'h02: begin div <= 32'h00532240; end
+            7'h03: begin div <= 32'h004e77c5; end
+            7'h04: begin div <= 32'h004a1054; end
+            7'h05: begin div <= 32'h0045e82b; end
+            7'h06: begin div <= 32'h0041fbbc; end
+            7'h07: begin div <= 32'h003e47ac; end
 
-            7'h08: div <= 32'h024bd838;
-            7'h09: div <= 32'h022ad9f8;
-            7'h0a: div <= 32'h020bb5c4;
-            7'h0b: div <= 32'h01ee5102;
-            7'h0c: div <= 32'h01d29294;
-            7'h0d: div <= 32'h01b862c8;
-            7'h0e: div <= 32'h019fab3e;
-            7'h0f: div <= 32'h018856d6;
+            7'h08: begin div <= 32'h003ac8d3; end
+            7'h09: begin div <= 32'h00377c32; end
+            7'h0a: begin div <= 32'h00345efa; end
+            7'h0b: begin div <= 32'h00316e80; end
+            7'h0c: begin div <= 32'h002ea842; end
+            7'h0d: begin div <= 32'h002c09e1; end
+            7'h0e: begin div <= 32'h00299120; end
+            7'h0f: begin div <= 32'h00273be2; end
 
-            7'h10: div <= 32'h017251a4;
-            7'h11: div <= 32'h015d88d6;
-            7'h12: div <= 32'h0149eaa8;
-            7'h13: div <= 32'h0137665c;
-            7'h14: div <= 32'h0125ec1c;
-            7'h15: div <= 32'h01156cfc;
-            7'h16: div <= 32'h0105dae2;
-            7'h17: div <= 32'h00f72882;
+            7'h10: begin div <= 32'h0025082a; end
+            7'h11: begin div <= 32'h0022f415; end
+            7'h12: begin div <= 32'h0020fdde; end
+            7'h13: begin div <= 32'h001f23d6; end
+            7'h14: begin div <= 32'h001d6469; end
+            7'h15: begin div <= 32'h001bbe19; end
+            7'h16: begin div <= 32'h001a2f7d; end
+            7'h17: begin div <= 32'h0018b740; end
 
-            7'h18: div <= 32'h00e9494a;
-            7'h19: div <= 32'h00dc3166;
-            7'h1a: div <= 32'h00cfd59e;
-            7'h1b: div <= 32'h00c42b6c;
-            7'h1c: div <= 32'h00b928d4;
-            7'h1d: div <= 32'h00aec46a;
-            7'h1e: div <= 32'h00a4f554;
-            7'h1f: div <= 32'h009bb32e;
+            7'h18: begin div <= 32'h00175421; end
+            7'h19: begin div <= 32'h001604f1; end
+            7'h1a: begin div <= 32'h0014c890; end
+            7'h1b: begin div <= 32'h00139df1; end
+            7'h1c: begin div <= 32'h00128415; end
+            7'h1d: begin div <= 32'h00117a0b; end
+            7'h1e: begin div <= 32'h00107eef; end
+            7'h1f: begin div <= 32'h000f91eb; end
 
-            7'h20: div <= 32'h0092f60e;
-            7'h21: div <= 32'h008ab67e;
-            7'h22: div <= 32'h0082ed72;
-            7'h23: div <= 32'h007b9440;
-            7'h24: div <= 32'h0074a4a5;
-            7'h25: div <= 32'h006e18b3;
-            7'h26: div <= 32'h0067eacf;
-            7'h27: div <= 32'h006215b6;
+            7'h20: begin div <= 32'h000eb235; end
+            7'h21: begin div <= 32'h000ddf0d; end
+            7'h22: begin div <= 32'h000d17bf; end
+            7'h23: begin div <= 32'h000c5ba0; end
+            7'h24: begin div <= 32'h000baa11; end
+            7'h25: begin div <= 32'h000b0278; end
+            7'h26: begin div <= 32'h000a6448; end
+            7'h27: begin div <= 32'h0009cef9; end
 
-            7'h28: div <= 32'h005c946a;
-            7'h29: div <= 32'h00576235;
-            7'h2a: div <= 32'h00527aaa;
-            7'h2b: div <= 32'h004dd997;
-            7'h2c: div <= 32'h00497b07;
-            7'h2d: div <= 32'h00455b3f;
-            7'h2e: div <= 32'h004176b9;
-            7'h2f: div <= 32'h003dca20;
+            7'h28: begin div <= 32'h0009420b; end
+            7'h29: begin div <= 32'h0008bd05; end
+            7'h2a: begin div <= 32'h00083f77; end
+            7'h2b: begin div <= 32'h0007c8f6; end
+            7'h2c: begin div <= 32'h0007591a; end
+            7'h2d: begin div <= 32'h0006ef86; end
+            7'h2e: begin div <= 32'h00068bdf; end
+            7'h2f: begin div <= 32'h00062dd0; end
 
-            7'h30: div <= 32'h003a5253;
-            7'h31: div <= 32'h00370c59;
-            7'h32: div <= 32'h0033f568;
-            7'h33: div <= 32'h00310adb;
-            7'h34: div <= 32'h002e4a34;
-            7'h35: div <= 32'h002bb11b;
-            7'h36: div <= 32'h00293d55;
-            7'h37: div <= 32'h0026eccb;
+            7'h30: begin div <= 32'h0005d508; end
+            7'h31: begin div <= 32'h0005813c; end
+            7'h32: begin div <= 32'h00053224; end
+            7'h33: begin div <= 32'h0004e77c; end
+            7'h34: begin div <= 32'h0004a105; end
+            7'h35: begin div <= 32'h00045e83; end
+            7'h36: begin div <= 32'h00041fbc; end
+            7'h37: begin div <= 32'h0003e47b; end
 
-            7'h38: div <= 32'h0024bd84;
-            7'h39: div <= 32'h0022ada0;
-            7'h3a: div <= 32'h0020bb5c;
-            7'h3b: div <= 32'h001ee510;
-            7'h3c: div <= 32'h001d2929;
-            7'h3d: div <= 32'h001b862c;
-            7'h3e: div <= 32'h0019fab4;
-            7'h3f: div <= 32'h0018856d;
+            7'h38: begin div <= 32'h0003ac8d; end
+            7'h39: begin div <= 32'h000377c3; end
+            7'h3a: begin div <= 32'h000345f0; end
+            7'h3b: begin div <= 32'h000316e8; end
+            7'h3c: begin div <= 32'h0002ea84; end
+            7'h3d: begin div <= 32'h0002c09e; end
+            7'h3e: begin div <= 32'h00029912; end
+            7'h3f: begin div <= 32'h000273be; end
 
-            7'h40: div <= 32'h0017251a;
-            7'h41: div <= 32'h0015d88e;
-            7'h42: div <= 32'h00149eab;
-            7'h43: div <= 32'h00137666;
-            7'h44: div <= 32'h00125ec2;
-            7'h45: div <= 32'h001156d0;
-            7'h46: div <= 32'h00105dae;
-            7'h47: div <= 32'h000f7288;
+            7'h40: begin div <= 32'h00025083; end
+            7'h41: begin div <= 32'h00022f41; end
+            7'h42: begin div <= 32'h00020fde; end
+            7'h43: begin div <= 32'h0001f23d; end
+            7'h44: begin div <= 32'h0001d647; end
+            7'h45: begin div <= 32'h0001bbe2; end
+            7'h46: begin div <= 32'h0001a2f8; end
+            7'h47: begin div <= 32'h00018b74; end
 
-            7'h48: div <= 32'h000e9495;
-            7'h49: div <= 32'h000dc316;
-            7'h4a: div <= 32'h000cfd5a;
-            7'h4b: div <= 32'h000c42b7;
-            7'h4c: div <= 32'h000b928d;
-            7'h4d: div <= 32'h000aec47;
-            7'h4e: div <= 32'h000a4f55;
-            7'h4f: div <= 32'h0009bb33;
+            7'h48: begin div <= 32'h00017542; end
+            7'h49: begin div <= 32'h0001604f; end
+            7'h4a: begin div <= 32'h00014c89; end
+            7'h4b: begin div <= 32'h000139df; end
+            7'h4c: begin div <= 32'h00012841; end
+            7'h4d: begin div <= 32'h000117a1; end
+            7'h4e: begin div <= 32'h000107ef; end
+            7'h4f: begin div <= 32'h0000f91f; end
 
-            7'h50: div <= 32'h00092f61;
-            7'h51: div <= 32'h0008ab68;
-            7'h52: div <= 32'h00082ed7;
-            7'h53: div <= 32'h0007b944;
-            7'h54: div <= 32'h00074a4a;
-            7'h55: div <= 32'h0006e18b;
-            7'h56: div <= 32'h00067ead;
-            7'h57: div <= 32'h0006215b;
+            7'h50: begin div <= 32'h0000eb23; end
+            7'h51: begin div <= 32'h0000ddf1; end
+            7'h52: begin div <= 32'h0000d17c; end
+            7'h53: begin div <= 32'h0000c5ba; end
+            7'h54: begin div <= 32'h0000baa1; end
+            7'h55: begin div <= 32'h0000b028; end
+            7'h56: begin div <= 32'h0000a645; end
+            7'h57: begin div <= 32'h00009cf0; end
 
-            7'h58: div <= 32'h0005c947;
-            7'h59: div <= 32'h00057623;
-            7'h5a: div <= 32'h000527ab;
-            7'h5b: div <= 32'h0004dd99;
-            7'h5c: div <= 32'h000497b0;
-            7'h5d: div <= 32'h000455b4;
-            7'h5e: div <= 32'h0004176c;
-            7'h5f: div <= 32'h0003dca2;
+            7'h58: begin div <= 32'h00009421; end
+            7'h59: begin div <= 32'h00008bd0; end
+            7'h5a: begin div <= 32'h000083f7; end
+            7'h5b: begin div <= 32'h00007c8f; end
+            7'h5c: begin div <= 32'h00007592; end
+            7'h5d: begin div <= 32'h00006ef8; end
+            7'h5e: begin div <= 32'h000068be; end
+            7'h5f: begin div <= 32'h000062dd; end
 
-            7'h60: div <= 32'h0003a525;
-            7'h61: div <= 32'h000370c6;
-            7'h62: div <= 32'h00033f56;
-            7'h63: div <= 32'h000310ae;
-            7'h64: div <= 32'h0002e4a3;
-            7'h65: div <= 32'h0002bb12;
-            7'h66: div <= 32'h000293d5;
-            7'h67: div <= 32'h00026ecd;
+            7'h60: begin div <= 32'h00005d51; end
+            7'h61: begin div <= 32'h00005814; end
+            7'h62: begin div <= 32'h00005322; end
+            7'h63: begin div <= 32'h00004e78; end
+            7'h64: begin div <= 32'h00004a10; end
+            7'h65: begin div <= 32'h000045e8; end
+            7'h66: begin div <= 32'h000041fc; end
+            7'h67: begin div <= 32'h00003e48; end
 
-            7'h68: div <= 32'h00024bd8;
-            7'h69: div <= 32'h00022ada;
-            7'h6a: div <= 32'h00020bb6;
-            7'h6b: div <= 32'h0001ee51;
-            7'h6c: div <= 32'h0001d293;
-            7'h6d: div <= 32'h0001b863;
-            7'h6e: div <= 32'h00019fab;
-            7'h6f: div <= 32'h00018857;
+            7'h68: begin div <= 32'h00003ac9; end
+            7'h69: begin div <= 32'h0000377c; end
+            7'h6a: begin div <= 32'h0000345f; end
+            7'h6b: begin div <= 32'h0000316e; end
+            7'h6c: begin div <= 32'h00002ea8; end
+            7'h6d: begin div <= 32'h00002c0a; end
+            7'h6e: begin div <= 32'h00002991; end
+            7'h6f: begin div <= 32'h0000273c; end
 
-            7'h70: div <= 32'h00017252;
-            7'h71: div <= 32'h00015d89;
-            7'h72: div <= 32'h000149eb;
-            7'h73: div <= 32'h00013766;
-            7'h74: div <= 32'h000125ec;
-            7'h75: div <= 32'h0001156d;
-            7'h76: div <= 32'h000105db;
-            7'h77: div <= 32'h0000f728;
+            7'h70: begin div <= 32'h00002508; end
+            7'h71: begin div <= 32'h000022f4; end
+            7'h72: begin div <= 32'h000020fe; end
+            7'h73: begin div <= 32'h00001f24; end
+            7'h74: begin div <= 32'h00001d64; end
+            7'h75: begin div <= 32'h00001bbe; end
+            7'h76: begin div <= 32'h00001a2f; end
+            7'h77: begin div <= 32'h000018b7; end
 
-            7'h78: div <= 32'h0000e949;
-            7'h79: div <= 32'h0000dc31;
-            7'h7a: div <= 32'h0000cfd6;
-            7'h7b: div <= 32'h0000c42b;
-            7'h7c: div <= 32'h0000b929;
-            7'h7d: div <= 32'h0000aec4;
-            7'h7e: div <= 32'h0000a4f5;
-            7'h7f: div <= 32'h00009bb3;
+            7'h78: begin div <= 32'h00001754; end
+            7'h79: begin div <= 32'h00001605; end
+            7'h7a: begin div <= 32'h000014c9; end
+            7'h7b: begin div <= 32'h0000139e; end
+            7'h7c: begin div <= 32'h00001284; end
+            7'h7d: begin div <= 32'h0000117a; end
+            7'h7e: begin div <= 32'h0000107f; end
+            7'h7f: begin div <= 32'h00000f92; end
+            default: begin div <= 32'hffffffff; end
         endcase
     end
     
-
-
+    // A-4
+    // Midi 'h45 ('d69)
+    // 440 Hz (per = 2.28ms), when CLK is 100 MHz
+    // 'h1_bbe4 ('d113_636)
 
     reg  [31:0] divider_cnt;
-    wire        divider_cnt_evnt = (divider_cnt == 0);
+    wire        divider_cnt_evnt = (divider_cnt == div);
     always @(posedge reset or posedge clk) begin
         if (reset) begin
             divider_cnt <= 0;
         end
-        else if (divider_cnt == div) begin
-        //else if (divider_cnt == 32'd113_636) begin
-            // 440 Hz, when CLK is 100 MHz
+        else if (divider_cnt == div || note_on_event || note_off_event) begin
             divider_cnt <= 0;
         end
         else begin
@@ -214,10 +218,10 @@ module gen_pulse (
         if (reset) begin
             sample_val <= 0;
         end
-        else if (midi_rdy && midi_cmd == `MIDI_CMD_NOTE_ON) begin
+        else if (note_on_event) begin
             sample_val <= {2'b00, midi_data1, 9'b0}; // Q2.16
         end
-        else if (midi_rdy && midi_cmd == `MIDI_CMD_NOTE_OFF) begin
+        else if (note_off_event) begin
             sample_val <= 0;
         end
         else if (divider_cnt_evnt) begin
