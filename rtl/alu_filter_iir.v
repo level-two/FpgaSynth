ef69fef902bef08bdc4e0ffdeb7accc9d61d265d
Yauheni Lychkouski <bredobulbulizator@gmail.com>
2017-04-05 08:25:53 +0300

    Implementation of the IIR filter using concept of parallel tasks

--- rtl/alu_filter_iir.v
+++ rtl/alu_filter_iir.v
@@ -23,7 +23,19 @@ module alu_filter_iir (
     output [43:0]            dsp_ins_flat
 );
 
+    // STORE SAMPLE_IN
+    reg signed [17:0] sample_in_reg;
+    always @(posedge reset or posedge clk) begin
+        if (reset) begin
+            sample_in_reg <= 18'h00000;
+        end
+        else if (sample_in_rdy) begin
+            sample_in_reg <= sample_in;
+        end
+    end
 
+
+    // TASKS
     localparam NOP            = 16'h0000;
     localparam MUL_C0_IN_AS   = 16'h0001;
     localparam MUL_CI_XYI_AC  = 16'h0002;
@@ -35,81 +47,192 @@ module alu_filter_iir (
     localparam REPEAT_4       = 16'h0080;
     localparam CAL_COEFS      = 16'h0100;
     localparam CAL_COEFS_WAIT = 16'h0200;
-    localparam END            = 16'h8000;
-
+    localparam JP_0           = 16'h0400;
+    localparam WAIT_IN        = 16'h0800;
 
 
     reg [15:0] tasks;
-
     always @(pc) begin
         case (pc)
-            4'h0   : tasks = PUSH_X_IN     |
+            4'h0   : tasks = WAIT_IN;
+            4'h1   : tasks = PUSH_X_IN     |
                              MOV_I_0       |
                              MUL_C0_IN_AS;
 
-            4'h1   : tasks = REPEAT_4      |
+            4'h2   : tasks = REPEAT_4      |
                              MUL_CI_XYI_AC |
                              INC_I;
 
-            4'h2   : tasks = NOP;
             4'h3   : tasks = NOP;
-            4'h4   : tasks = MOV_RES_AC    |
+            4'h4   : tasks = NOP;
+            4'h5   : tasks = MOV_RES_AC    |
                              PUSH_Y_AC;
 
-            4'h2   : tasks = CAL_COEFS;
-            4'h3   : tasks = CAL_COEFS_WAIT;
-            default: tasks = END;
+            4'h6   : tasks = CAL_COEFS;
+            4'h7   : tasks = CAL_COEFS_WAIT;
+            default: tasks = JP_0;
         endcase
     end
 
 
-
-
-
+    // PC
     reg [3:0] pc;
+    always @(posedge reset or posedge clk) begin
+        if (reset) begin
+            pc <= 4'h0;
+        end
+        else if (tasks & JP_0) begin
+            pc <= 4'h0;
+        end
+        else if((tasks & WAIT_IN  && !sample_in_rdy) ||      
+                (tasks & REPEAT_4 && repeat_st     ))
+        begin
+            pc <= pc;
+        end
+        else begin
+            pc <= pc + 4'h1;
+        end
+    end
 
 
-
-
-//--------------------------------------------------------
-// -------====== State Machine ======-------
-//-----------------------------------------------------
-    localparam ST_IDLE         = 0;
-    localparam ST_CALC         = 1;
-    localparam ST_WAIT_RESULT  = 2;
-    localparam ST_DONE         = 3;
-
-    reg [1:0] state;
-    reg [1:0] next_state;
+    // REPEAT
+    reg  [3:0] repeat_cnt;
+    wire [3:0] repeat_cnt_max = (tasks & REPEAT_4) ? 4'h3 : 4'h0;
+    wire       repeat_st      = (repeat_cnt != repeat_cnt_max);
 
     always @(posedge reset or posedge clk) begin
         if (reset) begin
-            state <= ST_IDLE;
+            repeat_cnt <= 4'h0;
+        end
+        else if (repeat_cnt == repeat_cnt_max) begin
+            repeat_cnt <= 4'h0;
         end
         else begin
-            state <= next_state;
+            repeat_cnt <= repeat_cnt + 4'h1;
         end
     end
 
-    always @(*) begin
-        next_state = state;
+
+    // INDEX REGISTER I
+    reg  [3:0] i_reg;
+    always @(posedge reset or posedge clk) begin
+        if (reset) begin
+            i_reg <= 4'h0;
+        end
+        else if (tasks & MOV_I_0) begin
+            i_reg <= 4'h0;
+        end
+        else if (tasks & INC_I) begin
+            i_reg <= i_reg + 4'h1;
+        end
+    end
+
+
+    // XY DELAY LINE
+    reg  signed [17:0] xy_dly_line[0:4];
+    always @(posedge reset or posedge clk) begin
+        if (reset) begin
+            xy_dly_line[0] <= 18'h00000;
+            xy_dly_line[1] <= 18'h00000;
+            xy_dly_line[2] <= 18'h00000;
+            xy_dly_line[3] <= 18'h00000;
+            xy_dly_line[4] <= 18'h00000;
+        end
+        else if (tasks & PUSH_X_IN) begin
+            xy_dly_line[0] <= sample_in_reg;
+            xy_dly_line[1] <= xy_dly_line[0];
+            xy_dly_line[2] <= xy_dly_line[1];
+        end
+        else if (tasks & PUSH_Y_AC) begin
+            xy_dly_line[3] <= p[33:16];
+            xy_dly_line[4] <= xy_dly_line[3];
+        end
+    end
+
+
+    // MOVE AC VALUE TO RESULTS
+    always @(posedge reset or posedge clk) begin
+        if (reset) begin
+            sample_out_rdy <= 1'b0;
+            sample_out     <= 18'h00000;
+        end
+        else if (tasks & MOV_RES_AC) begin
+            sample_out_rdy <= 1'b1;
+            sample_out     <= p[33:16];
+        end
+        else begin
+            sample_out_rdy <= 1'b0;
+            sample_out     <= 18'h00000;
+        end
+    end
+
+
+
+
+    wire signed [17:0] xyi = xy_dly_line[i_reg];
+
+    localparam MUL_C0_IN_AS   = 16'h0001;
+    localparam MUL_CI_XYI_AC  = 16'h0002;
+
+    // MUL TASKS
+    always @(posedge reset or posedge clk) begin
+        if (reset) begin
+            sample_out_rdy <= 1'b0;
+            sample_out     <= 18'h00000;
+        end
+        else if (tasks & MOV_RES_AC) begin
+            sample_out_rdy <= 1'b1;
+            sample_out     <= p[33:16];
+        end
+        else begin
+            sample_out_rdy <= 1'b0;
+            sample_out     <= 18'h00000;
+        end
+    end
+
+
+
+
+
+
+
+
+
+
+//----------------------------------
+// -------====== A,B ======-------
+//------------------------------
+    assign a = xy;
+    assign b = coefs[coef_sel];
+
+
+//-------------------------------------------------------------
+// -------====== ALU Operation mode controll ======-------
+//---------------------------------------------------------
+    always @(state) begin
+        opmode_x_in        = `DSP_X_IN_ZERO;
+        opmode_z_in        = `DSP_Z_IN_ZERO;
+        opmode_use_preadd  = 1'b0;
+        opmode_cryin       = 1'b0;
+        opmode_preadd_sub  = 1'b0;
+        opmode_postadd_sub = 1'b0;
+
         case (state)
-            ST_IDLE:
-                if (sample_in_rdy) begin
-                    next_state = ST_CALC;
-                end
-            ST_CALC:
-                if (coef_sel_last) begin
-                    next_state = ST_WAIT_RESULT;
-                end
-            ST_WAIT_RESULT:
-                if (calc_will_be_done) begin
-                    next_state = ST_DONE;
-                end
-            ST_DONE:
-                next_state = ST_IDLE;
+            ST_IDLE:           begin end
+            ST_CALC: begin
+                opmode_x_in = `DSP_X_IN_MULT;
+                opmode_z_in = `DSP_Z_IN_POUT;
+            end
+            ST_WAIT_RESULT:  begin end
+            ST_DONE:           begin end
         endcase
     end
+
+
+
+
+
+
 
 
 //---------------------------------------------
@@ -147,111 +270,6 @@ module alu_filter_iir (
     endgenerate
 
 
-    reg [2:0] coef_sel;
-    wire      coef_sel_last  = (coef_sel == 3'h4);
-    
-    always @(posedge reset or posedge clk) begin
-        if (reset) begin
-            coef_sel <= 0;
-        end
-        else if (state == ST_CALC && !coef_sel_last) begin
-            coef_sel <= coef_sel + 1;
-        end
-        else begin
-            coef_sel <= 0;
-        end
-    end
 
 
-//---------------------------------------------
-// -------====== Delay line ======-------
-//-----------------------------------------
-    reg  signed [17:0] xy_dly_line[0:4];
-    wire signed [17:0] xy = xy_dly_line[coef_sel];
-
-    always @(posedge reset or posedge clk) begin
-        if (reset) begin
-            xy_dly_line[0] <= 18'h00000;
-            xy_dly_line[1] <= 18'h00000;
-            xy_dly_line[2] <= 18'h00000;
-            xy_dly_line[3] <= 18'h00000;
-            xy_dly_line[4] <= 18'h00000;
-        end
-        else if (state == ST_IDLE && sample_in_rdy) begin
-            xy_dly_line[0] <= sample_in;
-            xy_dly_line[1] <= xy_dly_line[0];
-            xy_dly_line[2] <= xy_dly_line[1];
-        end
-        else if (state == ST_DONE) begin
-            xy_dly_line[3] <= p[33:16];
-            xy_dly_line[4] <= xy_dly_line[3];
-        end
-    end
-
-
-//----------------------------------
-// -------====== A,B ======-------
-//------------------------------
-    assign a = xy;
-    assign b = coefs[coef_sel];
-
-
-//-------------------------------------------------------------
-// -------====== ALU Operation mode controll ======-------
-//---------------------------------------------------------
-    always @(state) begin
-        opmode_x_in        = `DSP_X_IN_ZERO;
-        opmode_z_in        = `DSP_Z_IN_ZERO;
-        opmode_use_preadd  = 1'b0;
-        opmode_cryin       = 1'b0;
-        opmode_preadd_sub  = 1'b0;
-        opmode_postadd_sub = 1'b0;
-
-        case (state)
-            ST_IDLE:           begin end
-            ST_CALC: begin
-                opmode_x_in = `DSP_X_IN_MULT;
-                opmode_z_in = `DSP_Z_IN_POUT;
-            end
-            ST_WAIT_RESULT:  begin end
-            ST_DONE:           begin end
-        endcase
-    end
-
-
-//--------------------------------------------------------
-// -------====== Wait Result ======-------
-//----------------------------------------------------
-    reg [1:0] wait_clac_cnt;
-    wire      calc_will_be_done = (wait_clac_cnt == 2'h1);
-
-    always @(posedge reset or posedge clk) begin
-        if (reset) begin
-            wait_clac_cnt <= 0;
-        end
-        else if (state == ST_WAIT_RESULT) begin
-            wait_clac_cnt <= wait_clac_cnt + 1;
-        end
-        else begin
-            wait_clac_cnt <= 0;
-        end
-    end
-
-//-------------------------------------------
-// -------====== Output ======-------
-//------------------------------
-    always @(posedge reset or posedge clk) begin
-        if (reset) begin
-            sample_out_rdy <= 1'b0;
-            sample_out     <= 18'h00000;
-        end
-        else if (state == ST_DONE) begin
-            sample_out_rdy <= 1'b1;
-            sample_out     <= p[33:16];
-        end
-        else begin
-            sample_out_rdy <= 1'b0;
-            sample_out     <= 18'h00000;
-        end
-    end
 endmodule
