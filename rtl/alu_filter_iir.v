54d9e7f62f370a2b77a112c242a2e6c2c64d6418
Yauheni Lychkouski <bredobulbulizator@gmail.com>
2017-04-08 15:18:59 +0300

    Improved code style

--- rtl/alu_filter_iir.v
+++ rtl/alu_filter_iir.v
@@ -37,42 +37,38 @@ module alu_filter_iir (
 
 
     // TASKS
-    localparam NOP            = 16'h0000;
-    localparam MUL_CI_IN_AS   = 16'h0001;
-    localparam MUL_CI_XYI_AC  = 16'h0002;
-    localparam MOV_I_0        = 16'h0004;
-    localparam INC_I          = 16'h0008;
-    localparam MOV_RES_AC     = 16'h0010;
-    localparam PUSH_X_IN      = 16'h0020;
-    localparam PUSH_Y_AC      = 16'h0040;
-    localparam REPEAT_3       = 16'h0080;
-    localparam CAL_COEFS      = 16'h0100;
-    localparam CAL_COEFS_WAIT = 16'h0200;
-    localparam JP_0           = 16'h0400;
-    localparam WAIT_IN        = 16'h0800;
-
+    localparam [15:0] NOP            = 16'h0000;
+    localparam [15:0] MUL_CI_IN_AS   = 16'h0001;
+    localparam [15:0] MUL_CI_XYI_AC  = 16'h0002;
+    localparam [15:0] MOV_I_0        = 16'h0004;
+    localparam [15:0] INC_I          = 16'h0008;
+    localparam [15:0] MOV_RES_AC     = 16'h0010;
+    localparam [15:0] PUSH_X_IN      = 16'h0020;
+    localparam [15:0] PUSH_Y_AC      = 16'h0040;
+    localparam [15:0] REPEAT_3       = 16'h0080;
+    localparam [15:0] CAL_COEFS      = 16'h0100;
+    localparam [15:0] CAL_COEFS_WAIT = 16'h0200;
+    localparam [15:0] JP_0           = 16'h0400;
+    localparam [15:0] WAIT_IN        = 16'h0800;
+              
     reg [15:0] tasks;
     always @(pc) begin
         case (pc)
-            4'h0   : tasks = WAIT_IN;
-
+            4'h0   : tasks = WAIT_IN       ;
             4'h1   : tasks = PUSH_X_IN     |
                              MUL_CI_IN_AS  |
-                             INC_I;
-
+                             INC_I         ;
             4'h2   : tasks = REPEAT_3      |
                              MUL_CI_XYI_AC |
-                             INC_I;
-
+                             INC_I         ;
             4'h3   : tasks = MUL_CI_XYI_AC |
-                             MOV_I_0;
-
+                             MOV_I_0       ;
             4'h4   : tasks = REPEAT_3      |
-                             NOP;
-
+                             NOP           ;
             4'h5   : tasks = MOV_RES_AC    |
-                             PUSH_Y_AC;
-            default: tasks = JP_0;
+                             PUSH_Y_AC     |
+                             JP_0          ;
+            default: tasks = JP_0          ;
         endcase
     end
 
@@ -86,8 +82,8 @@ module alu_filter_iir (
         else if (tasks & JP_0) begin
             pc <= 4'h0;
         end
-        else if((tasks & WAIT_IN  && !sample_in_rdy) ||      
-                (tasks & REPEAT_3 && repeat_st     ))
+        else if ((tasks & WAIT_IN  && !sample_in_rdy) ||      
+                 (tasks & REPEAT_3 && repeat_st     ))
         begin
             pc <= pc;
         end
@@ -101,7 +97,6 @@ module alu_filter_iir (
     reg  [3:0] repeat_cnt;
     wire [3:0] repeat_cnt_max = (tasks & REPEAT_3) ? 4'h2 : 4'h0;
     wire       repeat_st      = (repeat_cnt != repeat_cnt_max);
-
     always @(posedge reset or posedge clk) begin
         if (reset) begin
             repeat_cnt <= 4'h0;
