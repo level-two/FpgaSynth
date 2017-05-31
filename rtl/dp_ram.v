55370fd06c431efc7320a899e1e61eb556b3ddd0
Yauheni Lychkouski <370903+level-two@users.noreply.github.com>
2017-06-01 00:05:39 +0300

    Refactored ram and fifo

--- rtl/dp_ram.v
+++ rtl/dp_ram.v
@@ -9,43 +9,31 @@
 // -----------------------------------------------------------------------------
 
 module dp_ram (
-    clk    ,
-    wr     ,
-    wr_addr,
-    wr_data,
-    rd     ,
-    rd_addr,
-    rd_data
+    input               clk,
+    input               wr,
+    input  [ADDR_W-1:0] wr_addr,
+    input  [DATA_W-1:0] wr_data,
+    input               rd,
+    input  [ADDR_W-1:0] rd_addr,
+    output [DATA_W-1:0] rd_data
 ); 
 
-parameter DATA_WIDTH = 8 ;
-parameter ADDR_WIDTH = 8 ;
-parameter RAM_DEPTH = 1 << ADDR_WIDTH;
-
-//--------------Input Ports----------------------- 
-//
-input clk;
-input wr;
-input [ADDR_WIDTH-1:0] wr_addr;
-input [DATA_WIDTH-1:0] wr_data;
-input rd;
-input [ADDR_WIDTH-1:0] rd_addr;
-output [DATA_WIDTH-1:0] rd_data;
+    parameter DATA_W    = 8;
+    parameter ADDR_W    = 8;
+    parameter RAM_DEPTH = 1 << ADDR_W;
 
 
-//--------------Internal variables---------------- 
-reg [DATA_WIDTH-1:0] mem [0:RAM_DEPTH-1];
+    //--------------Internal variables---------------- 
+    reg [DATA_W-1:0] mem [0:RAM_DEPTH-1];
 
 
-//--------------Code Starts Here------------------ 
-// Memory Write Block 
-always @ (posedge clk) begin
-    if (wr) begin
-        mem[wr_addr] <= wr_data;
+    // Memory Write Block 
+    always @ (posedge clk) begin
+        if (wr) begin
+            mem[wr_addr] <= wr_data;
+        end
     end
-end
 
-// Memory Read Block 
-assign rd_data = mem[rd_addr]; 
-
+    // Memory Read Block 
+    assign rd_data = mem[rd_addr]; 
 endmodule
