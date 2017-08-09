f7a3353b95a838eb9f8b86d52b7335b547f784ee
Yauheni Lychkouski <370903+level-two@users.noreply.github.com>
2017-08-10 00:12:15 +0300

    Tasks conception impl

--- rtl/tasks/task_idx_reg.v
+++ rtl/tasks/task_idx_reg.v
@@ -4,37 +4,33 @@
 // Unauthorized copying of this file, via any medium is strictly prohibited
 // Proprietary and confidential
 // -----------------------------------------------------------------------------
-// File: task_pc.v
-// Description: Task for the PC register
+// File: task_idx_reg.v
+// Description: Task for the register
 // -----------------------------------------------------------------------------
 
 
-module task_pc (
+module task_idx_reg (
     input                    clk,
     input                    reset,
     input      [TASKS_W-1:0] tasks,
-    input      [PC_W-1:0]    jp_addr,
-    output reg [PC_W-1:0]    pc_out
+    input      [REG_W-1:0]   reg_in,
+    output reg [REG_W-1:0]   reg_out
 );
 
-    parameter PC_W       = 4;
+    parameter REG_W      = 18;
     parameter TASKS_W    = 16;
-    parameter TASK_JP    = 0;
-    parameter TASK_JPS   = 0;
-
+    parameter TASK_LOAD  = 0;
+    parameter TASK_INC   = 0;
 
     always @(posedge reset or posedge clk) begin
         if (reset) begin
-            pc_out <= {PC_W{1'b0}};
+            reg_out <= {REG_W{1'b0}};
         end
-        else if (TASK_JP != 0 && tasks & TASK_JP) begin
-            pc_out <= jp_addr;
+        else if (tasks & TASK_LOAD) begin
+            reg_out <= reg_in;
         end
-        else if (TASK_JPS != 0 && tasks & TASK_JPS) begin
-            pc_out <= pc_out;
-        end
-        else begin
-            pc_out <= pc_out + PC_W'h1;
+        else if (tasks & TASK_INC) begin
+            reg_out <= reg_out + 'h1;
         end
     end
 endmodule
