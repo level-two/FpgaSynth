8586a8a082561cc292964dd0c664beadaf5bc8fb
Yauheni Lychkouski <bredobulbulizator@gmail.com>
2017-12-02 18:26:25 +0300

    SDRAM: Implementation of the Wishbone<->SDRAM Controller interface

--- rtl/sdram/sdram_wb.v
+++ rtl/sdram/sdram_wb.v
@@ -11,41 +11,64 @@
 
 module sdram_wb
 (
-    input             clk           ,
-    input             reset         ,
-
-    // WISHBONE SLAVE INTERFACE
-    input  [31:0]     wbs_address   ,
-    input  [15:0]     wbs_writedata ,
-    output [15:0]     wbs_readdata  ,
-    input             wbs_strobe    ,
-    input             wbs_cycle     ,
-    input             wbs_write     ,
-    output            wbs_ack       ,
-    //output          wbs_err       , // TBI
-
-    output [31:0]     sdram_addr    ,
-    output            sdram_wr      ,
-    output            sdram_rd      ,
-    output [15:0]     sdram_wr_data ,
-    input  [15:0]     sdram_rd_data ,
-    input             sdram_op_done  
-    //input           sdram_op_err  , // TBI
+    input             clk                 ,
+    input             reset               ,
+                                          
+    // WISHBONE SLAVE INTERFACE           
+    input  [31:0]     wbs_address         ,
+    input  [15:0]     wbs_writedata       ,
+    output [15:0]     wbs_readdata        ,
+    input             wbs_strobe          ,
+    input             wbs_cycle           ,
+    input             wbs_write           ,
+    output            wbs_ack             ,
+    //output          wbs_err             , // TBI
+                                          
+    output [31:0]     sdram_addr          , // Cur cmd adddr from CMD fifo
+    output            sdram_wr_rdn        , // Cur w/r from CMD fifo
+    output            sdram_cmd_rdy       , // CMD fifo is not emty
+    input             sdram_cmd_done      , // POP prev cmd from CMD fifo
+                                          
+    output [15:0]     sdram_wr_data       , // To fifo
+    input  [15:0]     sdram_rd_data       , // From SDRAM
+    output            sdram_access        ,
+    //input           sdram_op_err        , // TBI
 );
 
-    reg  wb_trans_dly;
-    wire wb_trans = wbs_strobe & wbs_cycle;
+    localparam FIFO_DW  = 32+1+16;
 
-    always @(posedge clk or posedge reset) begin
-        if (reset) wb_trans_dly <= 1'h0;
-        else       wb_trans_dly <= wb_trans;
-    end
+    wire   wbs_trans    = wbs_strobe & wbs_cycle;
+    assign wbs_stall    = fifo_full; 
+    assign wbs_ack      = sdram_cmd_done;
+    assign wbs_readdata = sdram_rd_data;
+    //assign wbs_err    = sdram_op_err; // TBI
 
-    assign sdram_rd      = wb_trans & ~wb_trans_dly & ~wbs_write;
-    assign sdram_wr      = wb_trans & ~wb_trans_dly & wbs_write;
-    assign wbs_ack       = sdram_op_done;
-    //assign wbs_err     = sdram_op_err; // TBI
-    assign sdram_addr    = wbs_address;
-    assign sdram_wr_data = wbs_writedata;
-    assign wbs_readdata  = sdram_rd_data;
+    wire               fifo_push;
+    wire               fifo_pop;
+    wire [FIFO_DW-1:0] fifo_data_in;
+    wire [FIFO_DW-1:0] fifo_data_out;
+    wire               fifo_empty;
+    wire               fifo_full;
+
+    assign sdram_access         = wbs_cycle;
+    assign fifo_push            = wbs_strobe && ~fifo_full;
+    assign fifo_pop             = sdram_cmd_done;
+    assign fifo_data_in         = {wbs_address, wbs_write, wbs_writedata};
+    assign sdram_cmd_rdy        = !fifo_empty;
+    assign {sdram_address, sdram_write, sdram_writedata} = fifo_data_out;
+
+    module syn_fifo #(
+        .DATA_W     (      FIFO_DW),
+        .ADDR_W     (            3),
+        .FIFO_DEPTH (            8))
+    (
+        .clk        (clk          ),
+        .rst        (reset        ),
+        .wr         (fifo_push    ),
+        .rd         (fifo_pop     ),
+        .data_in    (fifo_data_in ),
+        .data_out   (fifo_data_out),
+        .empty      (fifo_empty   ),
+        .full       (fifo_full    )
+    );    
 endmodule
