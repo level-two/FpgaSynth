7fdc5eaa6709d82724e53fd243662f9e9d8a9c8d
Yauheni <bredobulbulizator@gmail.com>
2018-01-31 00:34:25 +0300

    Fixes in arb_mul... not working

--- rtl/common/arb_mul.v
+++ rtl/common/arb_mul.v
@@ -39,7 +39,7 @@ module arb_mul
         input [PORTS_N-1:0] val;
         integer j;
         begin
-            first_one_bit = 32'h0;
+            first_one_bit = 'h0;
             for (j = 0; j < PORTS_N; j = j+1) begin
                 if (val[j] && first_one_bit == 0) begin
                     first_one_bit[j] = 1'b1;
@@ -64,45 +64,60 @@ module arb_mul
     reg               is_active[0:GNTS_N-1];
     reg [GNTS_W-1:0]  gnt_id   [0:PORTS_N-1];
 
-    integer i;
+
+    reg               is_acitive_new[0:GNTS_N-1];
+    reg [PORTS_N-1:0] gnt_new       [0:GNTS_N-1];
+
+    genvar i;
+    generate for (i=0; i<GNTS_N; i=i+1) begin : arb_logic_block
+        always @(*) begin : arb_logic_always
+            reg [PORTS_N-1:0] req_next_stage;
+            is_acitive_new[i]      = is_active[i];
+            gnt_new[i]             = gnt[i];
+
+            if (i == 0) begin
+                req[i]             = req_in;
+            end
+
+            req_next_stage         = ~gnt[i] &  req[i];
+
+            if (is_acitive_new[i] && (~req[i] & req_prev & gnt[i])) begin
+                is_acitive_new[i]  = 1'b0;
+                gnt_new[i]         = 'h0;
+                req_next_stage     = req[i];
+            end
+
+            if (!is_acitive_new[i] && req[i]) begin
+                is_acitive_new[i]  = 1'b1;
+                gnt_new[i]         = first_one_bit(req[i]);
+                req_next_stage     = req[i] & ~first_one_bit(req[i]);
+            end
+
+            if (i != GNTS_N-1) begin
+                req[i+1]           = req_next_stage; 
+            end
+        end
+    end endgenerate
+
+    integer ii;
 
     always @(posedge clk or posedge reset) begin
         if (reset) begin
-            for (i=0; i<GNTS_N; i=i+1) begin
-                is_active[i] <= 'h0;
-                gnt[i]       <= 'h0;
+            for (ii=0; ii<GNTS_N; ii=ii+1) begin
+                is_active[ii] <= 'h0;
+                gnt[ii]       <= 'h0;
             end
-            for (i=0; i<PORTS_N; i=i+1) begin
-                gnt_id[i] <= 'h0;
+            for (ii=0; ii<PORTS_N; ii=ii+1) begin
+                gnt_id[ii]    <= 'h0;
             end
-        end else begin
-            for (i=0; i<GNTS_N; i=i+1) begin : arb_impl_body
-                reg               is_acitive_new;
-                reg [PORTS_N-1:0] gnt_new;
-                reg [PORTS_N-1:0] req_next_stage;
+        end else begin : arb_impl_body
+            for (ii=0; ii<GNTS_N; ii=ii+1) begin
 
-                is_acitive_new = is_active[i];
-                gnt_new        = gnt[i];
-                req_next_stage = (i==0) ? req_in : req[i];
+                // TODO set using mask
+                gnt_id[first_one_pos(req[ii] & ~req_prev)] <= ii;
 
-                if (is_acitive_new && (~req[i] & req_prev & gnt[i])) begin
-                    // req deasserted
-                    is_acitive_new  = 1'b0;
-                    gnt_new         = 'h0;
-                    req_next_stage  = req[i];
-                end
-
-                if (!is_acitive_new && (req[i] & ~req_prev)) begin
-                    // req asserted
-                    is_acitive_new  = 1'b1;
-                    gnt_new         = first_one_bit(req[i] & ~req_prev);
-                    gnt_id[first_one_pos(req[i] & ~req_prev)] <= i;
-                    req_next_stage  = req[i] & ~gnt_new;
-                end
-
-                req[i+1]      = req_next_stage; 
-                is_active[i] <= is_acitive_new;
-                gnt[i]       <= gnt_new;
+                is_active[ii] <= is_acitive_new[ii];
+                gnt[ii]       <= gnt_new[ii];
             end
         end
     end
