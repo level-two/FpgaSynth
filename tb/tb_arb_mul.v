9ca5ae99dcc186dc71887330041e6a1ff4440541
Yauheni <bredobulbulizator@gmail.com>
2018-02-02 00:23:25 +0300

    improved arb_mul and tb_arb_mul

--- tb/tb_arb_mul.v
+++ tb/tb_arb_mul.v
@@ -17,8 +17,11 @@ module tb_arb_mul();
     real       A_CLK_PERIOD = (1 / (TIMESTEP * A_CLK_FREQ));
 
     localparam PORTS_N = 10;
-    localparam GNTS_N  = 2;
+    localparam GNTS_N  = 4;
     localparam GNTS_W  = 2;
+
+    localparam IDLE_DLY_MAX = 300;
+    localparam GNT_LEN_MAX  = 100;
 
     reg        clk;
     reg        reset;
@@ -49,24 +52,62 @@ module tb_arb_mul();
     );
 
 
+    genvar i;
+    generate for (i=0; i<PORTS_N; i=i+1) begin : client_inst_loop
+        initial begin : client_inst
+            integer idle_dly;
+            integer gnt_len;
+
+            forever begin
+                while (reset !== 1'b0) @(posedge clk);
+
+                idle_dly = $random() % IDLE_DLY_MAX;
+                gnt_len  = $random() % GNT_LEN_MAX;
+
+                req[i] <= 1'b0;
+                repeat (idle_dly       ) @(posedge clk);
+                req[i] <= 1'b1;
+                @(posedge clk);
+                while  (gnt[i] === 1'b0) @(posedge clk);
+                repeat (gnt_len        ) @(posedge clk);
+            end
+        end
+    end endgenerate
+
+
+    function integer ones_count;
+        input [PORTS_N-1:0] val;
+        integer i;
+        begin
+            ones_count = 0;
+            for (i=0; i<PORTS_N; i=i+1) begin
+                if (val[i]) ones_count = ones_count+1;
+            end
+        end
+    endfunction
+
+
     initial begin
         reset  <= 1;
-        req    <= 'h0;
-
         repeat (10) @(posedge clk);
         reset  <= 0;
-        repeat (10) @(posedge clk);
 
-        req    <= 'h0;
+        forever begin : dut_checks
+            integer j, k;
 
-        repeat (PORTS_N) begin
-            req <= {req, 2'b11};
-            repeat (10) @(posedge clk);
-        end
+            @(posedge clk);
+            if (ones_count(gnt) > GNTS_N) begin
+                $display("Error: Number of grants is greater than GNTS_N: 0x%0h", gnt);
+            end
 
-        repeat (PORTS_N-1) begin
-            req <= {req, 2'b00};
-            repeat (20) @(posedge clk);
+            for (j = 0; j < PORTS_N-1; j=j+1) begin
+                for (k = j+1; k < PORTS_N; k=k+1) begin
+                    if (gnt[j] && gnt[k] && gnt_id[GNTS_W*j+:GNTS_W] === gnt_id[GNTS_W*k+:GNTS_W]) begin
+                        $display("Error: Duplication of the gnt_id: gnt:0x%0h gnt_id:0x%0h gnt:0x%0h gnt_id:0x%0h",
+                            j, gnt_id[GNTS_W*j+:GNTS_W], k, gnt_id[GNTS_W*k+:GNTS_W]);
+                    end
+                end
+            end
         end
     end
 endmodule
